ab9d6fbf13406e9cb147c4a12c0c6b6e74d5fdd8
luna <luna@wvvw.me>
2023-10-26 22:11:51 +1030

    feat: health check

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@
 import { Axiom } from '@axiomhq/js';
 import { renderToStaticMarkup } from 'react-dom/server';
 import { App } from './components/app';
+import { getCommitHash } from './get-commit-hash';
+import semver from 'semver';
 
 const axiom = new Axiom({
   token: process.env.AXIOM_TOKEN!,
@@ -96,9 +98,38 @@ const getStatus = async () => {
   );
 };
 
+// Get the version of the current application
+const version = await import(`${process.cwd()}/package.json`)
+  .then((pkg) => semver.parse(pkg.version)?.major)
+  .catch(() => 'unknown');
+const releaseId = await import(`${process.cwd()}/package.json`)
+  .then((pkg) => `${pkg.version}+${getCommitHash(process.cwd())}`)
+  .catch(() => 'unknown');
+
 Bun.serve({
   port: process.env.PORT ?? 3000,
   async fetch(request, server) {
+    const url = new URL(request.url);
+    if (url.pathname === '/.well-known/health') {
+      const fields = {
+        version,
+        releaseId,
+        time: new Date().toISOString(),
+      };
+      return new Response(
+        JSON.stringify({
+          ...fields,
+          status: 'pass',
+        }),
+        {
+          status: 200,
+          headers: {
+            'content-type': 'application/health+json',
+          },
+        },
+      );
+    }
+
     const appStatuses = await getStatus();
     return new Response(
       '<!doctype html>' +
