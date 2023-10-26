c3b2345d3bd566b57d692359d3b7016b0ec2b97f
luna <luna@wvvw.me>
2023-10-27 01:39:17 +1030

    chore: update to health package

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,7 @@
-// In the background run a loop that once per minute checks all of my websites
-// This should check the /.well-known/health endpoint for eaach
-
 import { Axiom } from '@axiomhq/js';
 import { renderToStaticMarkup } from 'react-dom/server';
 import { App } from './components/app';
-import { getCommitHash } from './get-commit-hash';
-import semver from 'semver';
+import { health } from 'well-known-health';
 
 const axiom = new Axiom({
   token: process.env.AXIOM_TOKEN!,
@@ -67,68 +63,40 @@ const getStatus = async () => {
     | project URL=['event.url'], status=['event.status'], _time
     | sort by _time desc  
   `);
-  return (
-    result.matches
-      ?.map((match) => ({
-        time: match._time,
-        ...(match.data as {
+  return result.matches
+    ?.map((match) => ({
+      time: match._time,
+      ...(match.data as {
+        URL: string;
+        status: string;
+      }),
+    }))
+    .reduce(
+      (acc, curr) => {
+        if (acc[curr.URL]) {
+          return acc;
+        }
+
+        acc[curr.URL] = curr;
+        return acc;
+      },
+      {} as Record<
+        string,
+        {
+          time: string;
           URL: string;
           status: string;
-        }),
-      }))
-      // Dedupe all the URLs so we only have the most recent status
-      .reduce(
-        (acc, curr) => {
-          if (acc[curr.URL]) {
-            return acc;
-          }
-
-          acc[curr.URL] = curr;
-          return acc;
-        },
-        {} as Record<
-          string,
-          {
-            time: string;
-            URL: string;
-            status: string;
-          }
-        >,
-      )
-  );
+        }
+      >,
+    );
 };
-
-// Get the version of the current application
-const version = await import(`${process.cwd()}/package.json`)
-  .then((pkg) => semver.parse(pkg.version)?.major)
-  .catch(() => 'unknown');
-const releaseId = await import(`${process.cwd()}/package.json`)
-  .then((pkg) => `${pkg.version}+${getCommitHash(process.cwd())}`)
-  .catch(() => 'unknown');
 
 Bun.serve({
   port: process.env.PORT ?? 3000,
   async fetch(request) {
-    const url = new URL(request.url);
-    if (url.pathname === '/.well-known/health') {
-      const fields = {
-        version,
-        releaseId,
-        time: new Date().toISOString(),
-      };
-      return new Response(
-        JSON.stringify({
-          ...fields,
-          status: 'pass',
-        }),
-        {
-          status: 200,
-          headers: {
-            'content-type': 'application/health+json',
-          },
-        },
-      );
-    }
+    // Handle health checks
+    const response = health(request);
+    if (response) return response;
 
     const appStatuses = await getStatus();
     return new Response(
